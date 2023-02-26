319dd43a0d94c22b40ce4d633aefb92882f456a9
Amit Cesar <amitcesar27@gmail.com>
2023-02-26 03:50:08 -0300

    feat: threejs component

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,22 +1,67 @@
 import { Alert } from "react-native";
 import { Box, Center, Heading, VStack, Text } from "native-base";
 
+import {
+  Scene,
+  Mesh,
+  MeshBasicMaterial,
+  PerspectiveCamera,
+  BoxGeometry,
+} from "three";
+import ExpoTHREE, { Renderer } from "expo-three";
+import { ExpoWebGLRenderingContext, GLView } from "expo-gl";
+
 export function Home() {
+  const onContextCreate = async (gl: any) => {
+    const scene = new Scene();
+    const camera = new PerspectiveCamera(
+      75,
+      gl.drawingBufferWidth / gl.drawingBufferHeight,
+      0.1,
+      1000
+    );
+
+    gl.canvas = {
+      width: gl.drawingBufferWidth,
+      height: gl.drawingBufferHeight,
+    };
+
+    camera.position.z = 2;
+
+    const renderer = new Renderer({ gl });
+    // set size of buffer to be equal to drawing buffer width
+    renderer.setSize(gl.drawingBufferWidth, gl.drawingBufferHeight);
+
+    const geometry = new BoxGeometry(1, 1, 1);
+    const material = new MeshBasicMaterial({
+      color: "cyan",
+    });
+
+    const cube = new Mesh(geometry, material);
+    scene.add(cube);
+
+    const render = () => {
+      requestAnimationFrame(render);
+      // create rotate functionality
+      // rotate around x axis
+      cube.rotation.x += 0.01;
+
+      // rotate around y axis
+      cube.rotation.y += 0.01;
+
+      renderer.render(scene, camera);
+      gl.endFrameEXP();
+    };
+    render();
+  };
+
   return (
     <VStack bg="info.200" flex={1} alignItems="center">
       <Center>
-        <Heading
-          color={"gray.900"}
-          my={24}
-          mt={20}
-          fontSize="lg"
-          fontFamily="heading"
-        >
-          Home
-        </Heading>
-
-        <Box>Elemento 1</Box>
-        <Box>Elemento 3</Box>
+        <GLView
+          onContextCreate={onContextCreate}
+          style={{ width: 400, height: 400 }}
+        />
       </Center>
     </VStack>
   );
