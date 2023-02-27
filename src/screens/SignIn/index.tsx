ed5190a5fb0a9fe0dea657be9f552c34de8768d0
Amit Cesar <amitcesar27@gmail.com>
2023-02-27 20:46:45 -0300

    feat: login

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,32 +1,33 @@
 import { Alert, TouchableWithoutFeedback, Keyboard } from "react-native";
-import auth, { firebase } from "@react-native-firebase/auth";
-import firestore from "@react-native-firebase/firestore";
+import auth from "@react-native-firebase/auth";
 import { useNavigation } from "@react-navigation/native";
 
-import {
-  Box,
-  Center,
-  Heading,
-  VStack,
-  Text,
-  KeyboardAvoidingView,
-} from "native-base";
+import { Center, Heading, VStack, FormControl, HStack } from "native-base";
 
 import { Input } from "./Input";
 import { Button } from "./Button";
 
 import { AppStackRoutesParamList } from "../../Routes/app.routes";
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { Controller, useForm } from "react-hook-form";
 
 export type StackNavigation =
   NativeStackNavigationProp<AppStackRoutesParamList>;
 
+type LoginDataProps = {
+  email: string;
+  password: string;
+};
+
 export function SignIn() {
   const { navigate } = useNavigation<StackNavigation>();
+  const { control, handleSubmit, getValues } = useForm<LoginDataProps>();
 
   const handleLoginWithEmailAndPassword = () => {
+    const { email, password } = getValues();
+    console.log({ email, password });
     auth()
-      .signInWithEmailAndPassword("cesinhamaster@email.com", "cesar123")
+      .signInWithEmailAndPassword(email, password)
       .then((result) =>
         navigate("home", {
           currentUser: result.user,
@@ -45,18 +46,44 @@ export function SignIn() {
   return (
     <VStack bg="info.200" flex={1} alignItems="center" px={10} pt={20}>
       <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-        <Center>
-          <Heading color={"gray.900"} my={24}>
-            Acesse sua conta
-          </Heading>
+        <>
+          <Center>
+            <Heading color={"gray.900"} my={24}>
+              Acesse sua conta
+            </Heading>
+          </Center>
+          <FormControl>
+            <Controller
+              control={control}
+              name="email"
+              render={({ field: { onChange, value } }) => (
+                <Input
+                  placeholder="Email"
+                  onChangeText={onChange}
+                  value={value}
+                />
+              )}
+            />
 
-          <Input placeholder="Email" />
-          <Input placeholder="Senha" />
-          <Button
-            title="Fazer Login"
-            onPress={handleLoginWithEmailAndPassword}
-          />
-        </Center>
+            <Controller
+              control={control}
+              name="password"
+              render={({ field: { onChange, value } }) => (
+                <Input
+                  placeholder="password"
+                  onChangeText={onChange}
+                  value={value}
+                  type="password"
+                />
+              )}
+            />
+
+            <Button
+              title="Fazer Login"
+              onPress={handleSubmit(handleLoginWithEmailAndPassword)}
+            />
+          </FormControl>
+        </>
       </TouchableWithoutFeedback>
     </VStack>
   );
